7ed5b2858ec0868741457c3f1ff62bb070e8de08
parangdev <chathjyoun@gmail.com>
2025-06-19 16:08:26 +0900

    테스트중

--- src/main/java/test/tete2.java
+++ src/main/java/test/tete2.java
@@ -7,5 +7,6 @@ public static void main(String[] args) {
 	System.out.println("나는야 진짜바보");
 	System.out.println("나는지금바보다");
 	System.out.println("나는바보바보바보");
+	System.out.println("아렁머라ㅣㅁ어ㅏ러마");
 }
 }
